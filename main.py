44deb9056ca7830c5fcd6893fe03098c5c6fa4a9
Spencer Nisonoff <snisonoff@gmail.com>
2021-01-02 17:56:05 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import random
+
+def main():
+    todo = ["Grind Leetcode", "Apply for Jobs", "Work on Game", "Watch Unity Tutorials", "Work on React Tutorial"]
+    print(random.choice(todo))
+
+if __name__ == "__main__":
+    main()
